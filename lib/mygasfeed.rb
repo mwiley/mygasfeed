e941c264114b4fcc9fb3a4c610d418d8c96356ac
mwiley <masonwiley92@gmail.com>
2015-03-04 06:13:15 -0800

    loads api key from env variable

--- lib/mygasfeed.rb
+++ lib/mygasfeed.rb
@@ -5,10 +5,7 @@ Dir[File.dirname(__FILE__) + "/mygasfeed/resources/*.rb"].each { |f| require f  
 
 module Mygasfeed
 
-   #API_URL = 'http://api.mygasfeed.com'
-   #API_KEY = 'vtn4c3fycf'
-
-   API_URL = 'http://devapi.mygasfeed.com/'
-   API_KEY = 'rfej9napna'
+   API_URL = 'http://api.mygasfeed.com'
+   API_KEY = ENV["MYGASFEED_API_KEY"]
 
 end
